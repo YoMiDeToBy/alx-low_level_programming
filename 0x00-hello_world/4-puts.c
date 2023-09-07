0c18010da81d32b51e1d70a145c2eaa1fa2f7e19
YoMiDeToBy <orijataiye31@gmail.com>
2023-09-07 07:43:29 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-
 /**
-
- * main - this is the main put function
-
- * Return: 0
-
+ * main	- this	is	the	main	put	function
+ * Return	:	0
  */
-
 int main(void)
-
 {
-
         puts("\"Programming is like building a multilingual puzzle");
-
                 return (0);
-
 }
 
 	
