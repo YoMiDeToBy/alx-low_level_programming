718f88ae5d4ccd3a8eee998224c4db356c961c1a
YoMiDeToBy <orijataiye31@gmail.com>
2023-09-07 07:19:14 -0700

    put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - thid is the main put function
+ * Return: 0
+ */
+int maim(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
